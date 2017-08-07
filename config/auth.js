afa4c27c08dea840ca3d702e95249e181a5a3aee
BuildTools <robbereygel97@gmail.com>
2017-08-07 03:03:15 +0200

    Added database & auth config files.

---
+++ config/auth.js
@@ -0,0 +1,14 @@
+module.exports = {
+	'facebookAuth' : {
+		'clientID': '1358094254281606',
+		'clientSecret': '9d3e4955b1a2d48a1ffb47aa8d7ba481',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback',
+		'profileFields'   : ['id', 'name', 'email']
+	},
+
+	'googleAuth' : {
+		'clientID': 'enter client id here',
+		'clientSecret': 'enter client secret here',
+		'callbackURL': 'enter callback here'
+	}
+}
