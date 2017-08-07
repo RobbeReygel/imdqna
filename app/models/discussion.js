5181cdacc53a3bc700eb0ab52890b33f9bf6c32a
BuildTools <robbereygel97@gmail.com>
2017-08-07 18:38:01 +0200

    Added 'start discussion' page with mongo connection

---
+++ app/models/discussion.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var discussionSchema = new mongoose.Schema({
+    topic: String,
+});
+
+//var Discussion = mongoose.model("Discussion", discussionSchema);
+
+//module.exports = mongoose.model('Discussion', discussionSchema);
--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,11 @@
 var User = require('./models/user');
+var Discussion = require('./models/discussion');
+
+var mongoose = require('mongoose');
+var discussionSchema = new mongoose.Schema({
+    topic: String,
+});
+
 module.exports = function(app, passport){
 	app.get('/', function(req, res){
 		res.render('index.ejs');
@@ -38,6 +45,24 @@ module.exports = function(app, passport){
 	app.get('/logout', function(req, res){
 		req.logout();
 		res.redirect('/');
+	})
+
+	app.get('/discussion/create', isLoggedIn, function(req, res){
+		res.render('creatediscussion.ejs', { user: req.user });
+	});
+
+	app.post("/discussion/create", isLoggedIn, function(req,res){
+		//save topic to mongodb
+		var Discussion = mongoose.model("Discussion", discussionSchema);
+		var myData = new Discussion(req.body);
+		myData.save()
+        .then(item => {
+            res.redirect('/');
+        })
+        .catch(err => {
+            res.status(400).send("Unable to save to database");
+        });
+
 	})
 };
 
